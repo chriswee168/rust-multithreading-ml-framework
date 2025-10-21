022498360e99f3902c19a2d8042e6949bfc3c89a
Cy63rPh4n70m <103024336+Cy63rPh4n70m@users.noreply.github.com>
2025-10-21 18:38:09 +0800

    Use Arc and Mutex for direct edges
    Direct edges needs to have shared ownership between
    two neurons, and must be accessible by any thread.

--- rust_src/src/neural_net_src/neuron_src/core_deps.rs
+++ rust_src/src/neural_net_src/neuron_src/core_deps.rs
@@ -1,11 +1,15 @@
+use std::sync::{Arc, Mutex};
+
 use crate::neural_net_src::edge_src::direct_edge::DirectEdge;
 
 /// Struct that contains all attributes required for input,
 /// hidden and output neurons.
-pub struct NeuronAttr<'a>
+pub struct NeuronAttr
 {
-    forward_edges: Vec<&'a mut DirectEdge>,
-    backward_edges: Vec<&'a mut DirectEdge>,
+    // Contains Arc references to edges indicating which neurons the current
+    // neuron is connected to.
+    forward_edges: Vec<Arc<Mutex<DirectEdge>>>,
+    backward_edges: Vec<Arc<Mutex<DirectEdge>>>,
 
     neuron_level: u32,
     neuron_id: String,
