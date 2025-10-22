f0e7170f9062c714fba8aec3f00dbec0c8e50112
Cy63rPh4n70m <103024336+Cy63rPh4n70m@users.noreply.github.com>
2025-10-22 18:49:31 +0800

    Implement edge trait methods for input edge

--- rust_src/src/neural_net_src/edge_src/input_edge.rs
+++ rust_src/src/neural_net_src/edge_src/input_edge.rs
@@ -29,4 +29,45 @@ impl InputEdge
     }
 }
 
+impl EdgeTrait for InputEdge
+{
+    // Forward propagation.
+    fn forward(&self, input_val: f32) -> f32 {
+        // Perform y = w * x + b
+        if input_val >= 0.0
+        {
+            return input_val * self.attr.pos_weight + self.attr.bias
+        }
+        else
+        {
+            return input_val * self.attr.neg_weight + self.attr.bias
+        }
+    }
+
+    // Backpropagation through chain rule.
+    fn backward(&mut self, input_val: f32, gradient_val: f32, lr: f32) -> f32 {
+        // Derivatives
+        // y = w * x + b
+        // d_y/d_x = w
+        // d_y/d_w = x
+        // d_y/d_b = 1
+
+        // Update bias parameter.
+        self.attr.bias -= lr * gradient_val;
+
+        // Update weight parameter and calculate the gradient respect to input.
+        let input_gradient: f32;
+        if input_val >= 0.0
+        {
+            self.attr.pos_weight -= lr * gradient_val * input_val;
+            input_gradient = gradient_val * self.attr.pos_weight;
+        }
+        else
+        {
+            self.attr.neg_weight -= lr * gradient_val * input_val;
+            input_gradient = gradient_val * self.attr.neg_weight;
+        }
+
+        return input_gradient;
+    }
 }
