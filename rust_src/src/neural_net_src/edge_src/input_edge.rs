57bd43e369a95827bac61175d43fcf5d7301cbd5
Cy63rPh4n70m <103024336+Cy63rPh4n70m@users.noreply.github.com>
2025-10-22 17:50:17 +0800

    Create constructor for InputEdge

--- rust_src/src/neural_net_src/edge_src/input_edge.rs
+++ rust_src/src/neural_net_src/edge_src/input_edge.rs
@@ -1,6 +1,6 @@
 use std::sync::{Arc, Mutex};
 
-use crate::neural_net_src::{edge_src::core_deps::EdgeAttr, neuron_src::core_deps::NeuronTrait};
+use crate::neural_net_src::{edge_src::core_deps::{EdgeAttr, EdgeTrait}, neuron_src::core_deps::NeuronTrait};
 
 /// Connects an index of the input array with an input neuron.
 pub struct InputEdge
@@ -8,4 +8,25 @@ pub struct InputEdge
     attr: EdgeAttr, // Contains the essential attributes of an edge.
     input_array_index: usize,
     next_neuron: Arc<Mutex<Box<dyn NeuronTrait>>>
+}
+
+// Implement constructor.
+impl InputEdge
+{
+    pub fn new(
+        input_array_index: usize, 
+        next_neuron: Arc<Mutex<Box<dyn NeuronTrait>>>, 
+        weight_range: f32
+    ) -> Self
+    {
+        let edge_attr: EdgeAttr = EdgeAttr::new(weight_range);
+        return Self
+        {
+            attr: edge_attr,
+            input_array_index,
+            next_neuron
+        };        
+    }
+}
+
 }
