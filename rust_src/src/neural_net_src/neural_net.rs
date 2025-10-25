e8884a8cb55bec360b9abff39373a18ea9bd9061
Cy63rPh4n70m <103024336+Cy63rPh4n70m@users.noreply.github.com>
2025-10-25 11:39:27 +0800

    Use ArcNeuronTrait and ArcEdgeTrait
    Improve type readability.

--- rust_src/src/neural_net_src/neural_net.rs
+++ rust_src/src/neural_net_src/neural_net.rs
@@ -1,13 +1,13 @@
-use std::{collections::HashMap, sync::{Arc, Mutex}};
+use std::{collections::HashMap};
 
-use crate::neural_net_src::{edge_src::core_deps::EdgeTrait, neuron_src::core_deps::NeuronTrait};
+use crate::neural_net_src::{types_aliases::{ArcEdgeTrait, ArcNeuronTrait}};
 
 /// Define the main neural network struct.
 pub struct NeuralNet
 {
     // Contains all the neurons and the edges that connect them.
-    all_neurons: HashMap<String, Arc<Mutex<Box<dyn NeuronTrait>>>>,
-    all_edges: HashMap<String, Arc<Mutex<Box<dyn EdgeTrait>>>>
+    all_neurons: HashMap<String, ArcNeuronTrait>,
+    all_edges: HashMap<String, ArcEdgeTrait>
 }
 
 impl NeuralNet
