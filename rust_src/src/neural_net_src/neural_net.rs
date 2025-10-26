5afafb91430fc35be212c04240d7eb4ea04c6e2b
Cy63rPh4n70m <103024336+Cy63rPh4n70m@users.noreply.github.com>
2025-10-26 22:46:30 +0800

    Add thread_buffers attribute to NeuralNet

--- rust_src/src/neural_net_src/neural_net.rs
+++ rust_src/src/neural_net_src/neural_net.rs
@@ -1,13 +1,19 @@
-use std::{collections::HashMap};
+use std::{collections::{HashMap}, sync::{Arc, Mutex}};
 
-use crate::neural_net_src::{types_aliases::{ArcEdgeTrait, ArcNeuronTrait}};
+use crate::neural_net_src::types_aliases::{ArcEdgeTrait, ArcNeuronBufferVec, ArcNeuronTrait, NeuronBuffer};
 
 /// Define the main neural network struct.
 pub struct NeuralNet
 {
-    // Contains all the neurons and the edges that connect them.
+    // Contains an Arc reference to all neurons and edges in the
+    // neural net.
     all_neurons: HashMap<String, ArcNeuronTrait>,
-    all_edges: HashMap<String, ArcEdgeTrait>
+    all_edges: HashMap<String, ArcEdgeTrait>,
+
+    // Neuron buffers are used for Breadth First Search traversal
+    // during forward and backward propagation through neurons/edges.
+    // Each CPU thread uses its own buffer to reduce contention.
+    thread_buffers: ArcNeuronBufferVec
 }
 
 impl NeuralNet
