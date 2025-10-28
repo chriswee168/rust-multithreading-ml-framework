1a04b877009044cd5192e4f3db0450e584a4e724
Cy63rPh4n70m <103024336+Cy63rPh4n70m@users.noreply.github.com>
2025-10-28 10:40:57 +0800

    Create hidden_forward method
    hidden_forward method is used by input and hidden
    neurons during forward propagation. Stored as its own
    function for modularity.

--- rust_src/src/neural_net_src/neuron_src/hidden_neuron.rs
+++ rust_src/src/neural_net_src/neuron_src/hidden_neuron.rs
@@ -1,4 +1,4 @@
-use crate::neural_net_src::{neuron_src::core_deps::{NeuronAttr, NeuronTrait}, types_aliases::ArcEdgeTrait};
+use crate::neural_net_src::{neuron_src::{core_deps::{NeuronAttr, NeuronTrait}, forward::hidden_forward}, types_aliases::ArcEdgeTrait};
 
 /// Struct to define hidden neuron attributes and behaviour.
 pub struct HiddenNeuron
@@ -37,9 +37,10 @@ impl NeuronTrait for HiddenNeuron
     {
         self.attr.remove_edge(edge_index);
     }
+    /// Perform forward pass.
     fn forward(&mut self) 
     {
-        
+        hidden_forward(&self.attr);
     }
     fn backward(&mut self) 
     {
--- rust_src/src/neural_net_src/neuron_src/input_neuron.rs
+++ rust_src/src/neural_net_src/neuron_src/input_neuron.rs
@@ -1,4 +1,4 @@
-use crate::neural_net_src::{neuron_src::core_deps::{NeuronAttr, NeuronTrait}, types_aliases::ArcEdgeTrait};
+use crate::neural_net_src::{neuron_src::{core_deps::{NeuronAttr, NeuronTrait}, forward::{hidden_forward}}, types_aliases::ArcEdgeTrait};
 
 /// Struct to define input neuron attributes and behaviour.
 pub struct InputNeuron
@@ -37,9 +37,10 @@ impl NeuronTrait for InputNeuron
     {
         self.attr.remove_edge(edge_index);
     }
+    /// Perform forward pass.
     fn forward(&mut self) 
     {
-        
+        hidden_forward(&self.attr);
     }
     fn backward(&mut self) 
     {
