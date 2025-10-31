af9a1446f0d9456ea2f683f29f0a28b321575c74
Cy63rPh4n70m <103024336+Cy63rPh4n70m@users.noreply.github.com>
2025-10-31 22:20:21 +0800

    Add method to remove input edge

--- rust_src/src/neural_net_src/neural_net_impl_src/impl_remove_edges.rs
+++ rust_src/src/neural_net_src/neural_net_impl_src/impl_remove_edges.rs
@@ -1,6 +1,6 @@
 use std::sync::MutexGuard;
 
-use crate::neural_net_src::{edge_src::core_deps::EdgeTrait, neural_net::NeuralNet, neuron_src::core_deps::NeuronTrait, types_aliases::{ArcEdgeTrait, ArcNeuronTrait}};
+use crate::neural_net_src::{edge_src::core_deps::EdgeTrait, neural_net::NeuralNet, neuron_src::{core_deps::NeuronTrait, input_neuron}, types_aliases::{ArcEdgeTrait, ArcNeuronTrait}};
 
 impl NeuralNet
 {
@@ -10,6 +10,7 @@ impl NeuralNet
         let hidden_edge: ArcEdgeTrait = self.obtain_edge(edge_id.clone()).unwrap();
         let hidden_edge: MutexGuard<'_, Box<dyn EdgeTrait>> = hidden_edge.lock().unwrap();
         
+        // Get the two neurons that are connected via this edge.
         let neuron0: ArcNeuronTrait = hidden_edge.get_prev_neuron().unwrap();
         let neuron1: ArcNeuronTrait = hidden_edge.get_next_neuron().unwrap();
 
@@ -22,5 +23,22 @@ impl NeuralNet
 
         // Remove the edge from hidden edge hashmap.
         self.hidden_edges.remove(&edge_id);
+    }
+
+    /// Remove an input edge.
+    pub fn remove_input_edge(&mut self, edge_id: String)
+    {
+        let input_edge: ArcEdgeTrait = self.obtain_edge(edge_id.clone()).unwrap();
+        let input_edge: MutexGuard<'_, Box<dyn EdgeTrait>> = input_edge.lock().unwrap();
+
+        // Get the input neuron that uses this edge.
+        let input_neuron: ArcNeuronTrait = input_edge.get_next_neuron().unwrap();
+        let mut input_neuron: MutexGuard<'_, Box<dyn NeuronTrait>> = input_neuron.lock().unwrap();
+
+        // Remove edge arc from input neuron.
+        input_neuron.remove_backward_edge(edge_id.clone());
+
+        // Remove edge from input edge hashmap.
+        self.input_edges.remove(&edge_id);
     }
 }
