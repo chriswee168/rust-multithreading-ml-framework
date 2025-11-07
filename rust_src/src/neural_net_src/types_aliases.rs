f4da966579ad3a9e40e89f965f73272f79ae3403
Cy63rPh4n70m <103024336+Cy63rPh4n70m@users.noreply.github.com>
2025-11-07 18:09:22 +0800

    Replace Mutex with RwLock
    More efficient for work stealing.

--- rust_src/src/neural_net_src/types_aliases.rs
+++ rust_src/src/neural_net_src/types_aliases.rs
@@ -1,4 +1,4 @@
-use std::{collections::VecDeque, sync::{Arc, Mutex}};
+use std::{collections::VecDeque, sync::{Arc, Mutex, RwLock}};
 
 use crate::neural_net_src::{edge_src::core_deps::EdgeTrait, neuron_src::core_deps::NeuronTrait};
 
@@ -10,4 +10,4 @@ pub type ArcEdgeTrait = Arc<Mutex<Box<dyn EdgeTrait>>>;
 
 // Type aliases for neural network.
 pub type NeuronBuffer = VecDeque<ArcNeuronTrait>; // Used to keep a buffer/queue of neurons for a CPU thread to work on.
-pub type ArcNeuronBufferVec = Arc<Vec<Mutex<NeuronBuffer>>>; // Contain a vector of neuron buffers for each CPU thread. 
+pub type ArcNeuronBufferVec = Arc<Vec<RwLock<NeuronBuffer>>>; // Contain a vector of neuron buffers for each CPU thread. 
