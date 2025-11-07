2fde404f8a0c65a6adc1254c7f709560aa71cc21
Cy63rPh4n70m <103024336+Cy63rPh4n70m@users.noreply.github.com>
2025-11-07 18:44:49 +0800

    Replace Mutex with RwLock

--- rust_src/src/neural_net_src/neural_net.rs
+++ rust_src/src/neural_net_src/neural_net.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, sync::{Arc, Mutex}, thread::JoinHandle};
+use std::{collections::HashMap, sync::{Arc, Mutex, RwLock}, thread::JoinHandle};
 
 use crate::neural_net_src::types_aliases::{ArcEdgeTrait, ArcNeuronBufferVec, ArcNeuronTrait, NeuronBuffer};
 
@@ -28,10 +28,10 @@ impl NeuralNet
     pub fn new(num_threads: usize) -> Self
     {
         // Create thread buffers.
-        let mut thread_buffers: Vec<Mutex<NeuronBuffer>> = Vec::with_capacity(num_threads);
+        let mut thread_buffers: Vec<RwLock<NeuronBuffer>> = Vec::with_capacity(num_threads);
         for _ in 0..num_threads - 1 // One thread is already used by main program.
         {
-            thread_buffers.push(Mutex::new(NeuronBuffer::new()));
+            thread_buffers.push(RwLock::new(NeuronBuffer::new()));
         }
         let thread_buffer_arc: ArcNeuronBufferVec = Arc::new(thread_buffers);
         
