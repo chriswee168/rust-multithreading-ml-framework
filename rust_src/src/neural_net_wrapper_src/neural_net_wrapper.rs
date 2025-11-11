acaa3ec42481bbf5b7c413694e73105664ca88d2
Cy63rPh4n70m <103024336+Cy63rPh4n70m@users.noreply.github.com>
2025-11-11 17:13:30 +0800

    Make NeuralNetWrapper attributes public

--- rust_src/src/neural_net_wrapper_src/neural_net_wrapper.rs
+++ rust_src/src/neural_net_wrapper_src/neural_net_wrapper.rs
@@ -5,26 +5,26 @@ use crate::neural_net_src::{neural_net::NeuralNet, thread_src::main_thread_fn::m
 pub struct NeuralNetWrapper
 {
     // Struct of the neural network.
-    neural_net: NeuralNet,
+    pub neural_net: NeuralNet,
 
     // Neuron buffers are used for Breadth First Search traversal
     // during forward and backward propagation through neurons/edges.
     // Each CPU thread uses its own buffer to reduce contention.
-    thread_buffers: ArcNeuronBufferVec,
+    pub thread_buffers: ArcNeuronBufferVec,
 
     // Contains handles of each thread for thread management.
-    thread_handles: Vec<JoinHandle<()>>,
+    pub thread_handles: Vec<JoinHandle<()>>,
 
     // Atomic boolean to indicate whether threads should perform forward
     // or backpropagation. (true by default)
-    traverse_forward: Arc<AtomicBool>,
+    pub traverse_forward: Arc<AtomicBool>,
 
     // RwLocked input vectors for neural network to process
     // and RwLocked output vectors to store outputs.
-    input_rwlock_vec: Arc<RwLock<Vec<f32>>>,
-    input_rwlock_grad_vec: Arc<RwLock<Vec<f32>>>,
-    output_rwlock_vec: Arc<RwLock<Vec<f32>>>,
-    output_rwlock_grad_vec: Arc<RwLock<Vec<f32>>>,
+    pub input_rwlock_vec: Arc<RwLock<Vec<f32>>>,
+    pub input_rwlock_grad_vec: Arc<RwLock<Vec<f32>>>,
+    pub output_rwlock_vec: Arc<RwLock<Vec<f32>>>,
+    pub output_rwlock_grad_vec: Arc<RwLock<Vec<f32>>>,
     
 }
 impl NeuralNetWrapper
