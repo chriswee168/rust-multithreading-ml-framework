7c65f6b00208b87fd02235acdd5b9f0c473f5de2
Cy63rPh4n70m <103024336+Cy63rPh4n70m@users.noreply.github.com>
2025-11-13 16:56:23 +0800

    Revert "Use only one shared condvar and mutex"
    This reverts commit 880d85d5b2f5a9065eedd47992decc9907e0c8fe.

--- rust_src/src/neural_net_wrapper_src/neural_net_wrapper.rs
+++ rust_src/src/neural_net_wrapper_src/neural_net_wrapper.rs
@@ -12,10 +12,6 @@ pub struct NeuralNetWrapper
     // Each CPU thread uses its own buffer to reduce contention.
     pub thread_buffers: ArcNeuronBufferVec,
 
-    // Shared thread condvar to pause all threads when adding input neurons
-    // to their buffers.
-    pub thread_condvar: Arc<(Condvar, Mutex<bool>)>, 
-
     // Contains handles of each thread for thread management.
     pub thread_handles: Vec<JoinHandle<()>>,
 
@@ -42,7 +38,6 @@ impl NeuralNetWrapper
             neural_net: NeuralNet::new(),
             
             thread_buffers: thread_buffer_arc,
-            thread_condvar: Arc::new((Condvar::new(), Mutex::new(false))),
             thread_handles: Vec::new(),
             traverse_forward: Arc::new(AtomicBool::new(true)),
 
@@ -57,12 +52,16 @@ impl NeuralNetWrapper
     pub fn spawn_threads(&mut self, num_threads: usize)
     {
         // Create thread buffers.
-        let mut thread_buffers: Vec<RwLock<NeuronBuffer>> = 
+        let mut thread_buffers: Vec<(Condvar, Mutex<bool>, RwLock<NeuronBuffer>)> = 
             Vec::with_capacity(num_threads);
 
         for _ in 0..num_threads
         {
-            thread_buffers.push(RwLock::new(NeuronBuffer::new()));
+            thread_buffers.push((
+                Condvar::new(),
+                Mutex::new(false),
+                RwLock::new(NeuronBuffer::new())
+            ));
         }
 
         // Re-assign number of thread buffers.
