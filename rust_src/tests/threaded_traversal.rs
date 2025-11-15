83faa87d3874bee8f1a49327f2bfa6dff415b518
Cy63rPh4n70m <103024336+Cy63rPh4n70m@users.noreply.github.com>
2025-11-15 21:45:30 +0800

    Connect input neurons to output neurons
    Add neuron edge connections that can bypass other neurons
    to test connectivity during propagation.

--- rust_src/tests/threaded_traversal.rs
+++ rust_src/tests/threaded_traversal.rs
@@ -58,6 +58,16 @@ fn threaded_traversal()
             );
         }
 
+        // Also join each input neuron with each output neuron.
+        for (output_neuron_name, _)  in &output_neuron_names
+        {
+            neural_net.join_neurons(
+                input_neuron_name, output_neuron_name, 
+                rand_id_gen(10), 1.0, 1.0
+            );
+        }
+
+        // Create input edges to obtain values from input vector.
         for i in 0..4
         {
             neural_net.add_input_edge(
@@ -79,13 +89,14 @@ fn threaded_traversal()
         }
     }
 
+    // Create input edges to send values to output vector.
     for (output_neuron_name, _)  in &output_neuron_names
     {
         for i in 0..4
         {
             neural_net.add_output_edge(
-            output_neuron_name, rand_id_gen(10), 
-            i, 1.0, 1.0
+                output_neuron_name, rand_id_gen(10), 
+                i, 1.0, 1.0
             );
         }
     }
@@ -101,7 +112,7 @@ fn threaded_traversal()
     let sample_input_vec: Vec<f32> = vec![1.0, 2.0, 3.0, 4.0];
     neural_net.set_input_vec(sample_input_vec);
     neural_net.forward();
-    assert_eq!(vec![160.0, 160.0, 160.0, 160.0], *neural_net.get_output_vec());
+    assert_eq!(vec![240.0, 240.0, 240.0, 240.0], *neural_net.get_output_vec());
 
     // Check each neuron has the correct values accumulated.
     for (_, neuron) in &neural_net.neural_net.input_neurons
@@ -119,6 +130,6 @@ fn threaded_traversal()
     for (_, neuron) in &neural_net.neural_net.output_neurons
     {
         let guard = neuron.lock().unwrap();
-        assert_eq!(80.0, guard.get_sum(true));
+        assert_eq!(120.0, guard.get_sum(true));
     }
 }
