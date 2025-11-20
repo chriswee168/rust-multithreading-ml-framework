0bd15fdae9e5dbca3ba597eaef7717ebf5fe913b
Cy63rPh4n70m <103024336+Cy63rPh4n70m@users.noreply.github.com>
2025-11-20 20:05:14 +0800

    Create methods to add neurons

---
+++ rust_src/src/extern_funcs_src/add_neurons.rs
@@ -0,0 +1,69 @@
+use std::{ffi::c_void, u32::MAX};
+
+use crate::{neural_net_src::{
+    neuron_src::create_neuron::create_neuron, rand_id_gen::rand_id_gen, 
+    types_aliases::ArcNeuronTrait}, neural_net_wrapper_src::neural_net_wrapper::NeuralNetWrapper};
+
+/// Add input neuron.
+#[unsafe(no_mangle)]
+pub extern "C" fn add_input_neuron_ext(
+    nn_vp: *mut c_void, id_len: usize, 
+    max_edges: usize
+)
+{
+    unsafe
+    {
+        let nn_ptr: *mut NeuralNetWrapper = nn_vp as *mut NeuralNetWrapper;
+        let rand_id: String = rand_id_gen(id_len);
+        let neuron: ArcNeuronTrait = create_neuron(
+            max_edges, max_edges, 
+            0, "input", 
+            (*nn_ptr).edge_counter.clone()
+        );
+        
+        (*nn_ptr).add_input_neuron(rand_id, neuron);
+    }
+}
+
+
+/// Add hidden neuron.
+#[unsafe(no_mangle)]
+pub extern "C" fn add_hidden_neuron_ext(
+    nn_vp: *mut c_void, id_len: usize, 
+    max_edges: usize, neuron_level: u32
+)
+{
+    unsafe
+    {
+        let nn_ptr: *mut NeuralNetWrapper = nn_vp as *mut NeuralNetWrapper;
+        let rand_id: String = rand_id_gen(id_len);
+        let neuron: ArcNeuronTrait = create_neuron(
+            max_edges, max_edges, 
+            neuron_level, "output", 
+            (*nn_ptr).edge_counter.clone()
+        );
+        
+        (*nn_ptr).add_hidden_neuron(rand_id, neuron);
+    }
+}
+
+/// Add output neuron.
+#[unsafe(no_mangle)]
+pub extern "C" fn add_output_neuron_ext(
+    nn_vp: *mut c_void, id_len: usize, 
+    max_edges: usize,
+)
+{
+    unsafe
+    {
+        let nn_ptr: *mut NeuralNetWrapper = nn_vp as *mut NeuralNetWrapper;
+        let rand_id: String = rand_id_gen(id_len);
+        let neuron: ArcNeuronTrait = create_neuron(
+            max_edges, max_edges, 
+            MAX, "output", 
+            (*nn_ptr).edge_counter.clone()
+        );
+        
+        (*nn_ptr).add_output_neuron(rand_id, neuron);
+    }
+}
--- rust_src/src/extern_funcs_src/mod.rs
+++ rust_src/src/extern_funcs_src/mod.rs
@@ -1,3 +1,4 @@
 pub mod create_nn;
 pub mod propagate;
 pub mod init_io_neuron_buffers;
+pub mod add_neurons;
