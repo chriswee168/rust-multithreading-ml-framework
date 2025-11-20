a2ff6d9048fa1832fe8db739c867f37632b1ed91
Cy63rPh4n70m <103024336+Cy63rPh4n70m@users.noreply.github.com>
2025-11-20 10:57:28 +0800

    Add methods to send vectors to neural net

--- rust_src/src/extern_funcs_src/propagate.rs
+++ rust_src/src/extern_funcs_src/propagate.rs
@@ -78,3 +78,26 @@ pub extern "C" fn init_output_vecs_ext(nn_vp: *mut c_void, out_dim: usize)
     }
 }
 
+/// Set the input vector.
+#[unsafe(no_mangle)]
+pub extern "C" fn set_input_vec_ext(nn_vp: *mut c_void, input_arr: *mut f32, in_dim: usize)
+{
+    unsafe
+    {
+        let vector: Vec<f32> = std::slice::from_raw_parts_mut(input_arr, in_dim).to_vec();
+        let nn_ptr: *mut NeuralNetWrapper = nn_vp as *mut NeuralNetWrapper;
+        (*nn_ptr).set_input_vec(vector);
+    }
+}
+
+/// Set the output gradient vector.
+#[unsafe(no_mangle)]
+pub extern "C" fn set_output_grad_vec_ext(nn_vp: *mut c_void, out_dim: usize)
+{
+    unsafe
+    {
+        let nn_ptr: *mut NeuralNetWrapper = nn_vp as *mut NeuralNetWrapper;
+        (*nn_ptr).init_output_vecs(out_dim);
+    }
+}
+
