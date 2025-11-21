2a437c58153993a4e09e47d570910f5b5969af5c
Cy63rPh4n70m <103024336+Cy63rPh4n70m@users.noreply.github.com>
2025-11-21 09:43:00 +0800

    Add methods to add input and output edges

--- rust_src/src/extern_funcs_src/join_neurons.rs
+++ rust_src/src/extern_funcs_src/join_neurons.rs
@@ -24,3 +24,44 @@ pub fn join_neurons_ext(
         
     }
 }
+
+/// Add an input edge to an input neuron.
+#[unsafe(no_mangle)]
+pub extern "C" fn add_input_edge_ext(
+    nn_vp: *mut c_void, input_neuron_id: *mut c_char, arr_idx: usize,
+    neg_weight: f32, pos_weight: f32
+)
+{
+    unsafe
+    {
+        let nn_ptr: *mut NeuralNetWrapper = nn_vp as *mut NeuralNetWrapper;
+        let neuron_id_str: &str = CStr::from_ptr(input_neuron_id).to_str().unwrap();
+        let edge_id: String = arr_idx.to_string() + "_" + neuron_id_str;
+
+        (*nn_ptr).add_input_edge(
+            neuron_id_str, edge_id, 
+            arr_idx, neg_weight, pos_weight
+        );
+    }
+}
+
+
+/// Add an output edge to an output neuron.
+#[unsafe(no_mangle)]
+pub extern "C" fn add_output_edge_ext(
+    nn_vp: *mut c_void, output_neuron_id: *mut c_char, arr_idx: usize,
+    neg_weight: f32, pos_weight: f32
+)
+{
+    unsafe
+    {
+        let nn_ptr: *mut NeuralNetWrapper = nn_vp as *mut NeuralNetWrapper;
+        let neuron_id_str: &str = CStr::from_ptr(output_neuron_id).to_str().unwrap();
+        let edge_id: String = neuron_id_str.to_string() + "_" + arr_idx.to_string().as_str();
+
+        (*nn_ptr).add_output_edge(
+            neuron_id_str, edge_id, 
+            arr_idx, neg_weight, pos_weight
+        );
+    }
+}
