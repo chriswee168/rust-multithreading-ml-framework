7539763390b8b7449bffedacb8872ebe60fb2c79
Cy63rPh4n70m <103024336+Cy63rPh4n70m@users.noreply.github.com>
2025-11-22 10:25:18 +0800

    Move neuron_level parameter
    Should be under hidden neuron.

--- rust_src/src/extern_funcs_src/add_neurons.rs
+++ rust_src/src/extern_funcs_src/add_neurons.rs
@@ -29,26 +29,6 @@ pub extern "C" fn add_input_neuron_ext(
 #[unsafe(no_mangle)]
 pub extern "C" fn add_hidden_neuron_ext(
     nn_vp: *mut c_void, id: *mut c_char, 
-    max_edges: usize
-)
-{
-    unsafe
-    {
-        let nn_ptr: *mut NeuralNetWrapper = nn_vp as *mut NeuralNetWrapper;
-        let id_str: String = CStr::from_ptr(id).to_str().unwrap().to_string();
-        let neuron: ArcNeuronTrait = create_neuron(
-            max_edges, max_edges, 
-            "output", MAX
-        );
-        
-        (*nn_ptr).add_hidden_neuron(id_str, neuron);
-    }
-}
-
-/// Add output neuron.
-#[unsafe(no_mangle)]
-pub extern "C" fn add_output_neuron_ext(
-    nn_vp: *mut c_void, id: *mut c_char, 
     max_edges: usize, neuron_level: u32
 )
 {
@@ -59,6 +39,26 @@ pub extern "C" fn add_output_neuron_ext(
         let neuron: ArcNeuronTrait = create_neuron(
             max_edges, max_edges, 
             "output", neuron_level
+        );
+        
+        (*nn_ptr).add_hidden_neuron(id_str, neuron);
+    }
+}
+
+/// Add output neuron.
+#[unsafe(no_mangle)]
+pub extern "C" fn add_output_neuron_ext(
+    nn_vp: *mut c_void, id: *mut c_char, 
+    max_edges: usize
+)
+{
+    unsafe
+    {
+        let nn_ptr: *mut NeuralNetWrapper = nn_vp as *mut NeuralNetWrapper;
+        let id_str: String = CStr::from_ptr(id).to_str().unwrap().to_string();
+        let neuron: ArcNeuronTrait = create_neuron(
+            max_edges, max_edges, 
+            "output", MAX
         );
         
         (*nn_ptr).add_output_neuron(id_str, neuron);
