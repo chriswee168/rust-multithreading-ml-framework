92f3268b2989e583269856ec5dee037decd780d7
Cy63rPh4n70m <103024336+Cy63rPh4n70m@users.noreply.github.com>
2025-11-25 17:26:45 +0800

    Add function to create a neural net

--- python_src/config/ctypes_funcs.py
+++ python_src/config/ctypes_funcs.py
@@ -10,4 +10,9 @@ def get_ctypes_funcs(lib_path: str) -> dict[str, C._NamedFuncPointer]:
 
     all_funcs: dict[str, C._NamedFuncPointer] = {}
 
+    # Create neural network.
+    create_nn = rust_backend_lib.create_nn_ext
+    create_nn.restype = C.c_void_p
+    all_funcs.update({"create_nn": create_nn})
+
     return all_funcs
