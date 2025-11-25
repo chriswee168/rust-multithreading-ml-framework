aadd15c151ea25dc51c0b023110fc0d6b05510c1
Cy63rPh4n70m <103024336+Cy63rPh4n70m@users.noreply.github.com>
2025-11-25 18:24:02 +0800

    Create the ctype functions for neuron buffers

---
+++ python_src/config/ctypes_funcs/init_neuron_buffers.py
@@ -0,0 +1,17 @@
+import ctypes as C
+from ctypes import CDLL
+
+def get_neuron_buffer_funcs(rust_backend_lib: CDLL, all_funcs: dict[str, C._NamedFuncPointer]):
+    """
+    Initializes the initial neuron buffers for the neural network to
+    use when performing forward and backpropagation.
+    Args: nn_vp
+    """
+    
+    init_forward_buffer = rust_backend_lib.init_forward_buffers_ext
+    init_forward_buffer.argtypes = [C.c_void_p]
+    all_funcs.update({"init_forward_buffer": init_forward_buffer})
+
+    init_backward_buffer = rust_backend_lib.init_backward_buffers_ext
+    init_backward_buffer.argtypes = [C.c_void_p]
+    all_funcs.update({"init_backward_buffer": init_backward_buffer})
