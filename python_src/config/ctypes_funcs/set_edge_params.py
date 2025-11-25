edb48de4ec6306b679f0973fd158d8c69555bbeb
Cy63rPh4n70m <103024336+Cy63rPh4n70m@users.noreply.github.com>
2025-11-26 01:16:57 +0800

    Create ctypes function for setting edge parameters

---
+++ python_src/config/ctypes_funcs/set_edge_params.py
@@ -0,0 +1,12 @@
+import ctypes as C
+from ctypes import CDLL
+
+def get_set_edge_params_func(rust_backend_lib: CDLL, all_funcs: dict[str, C._NamedFuncPointer]):
+    """
+    Function to change the positive and negative parameters of a random edge.
+    """
+
+    # Args: nn_vp, pos_param, neg_param
+    set_rand_edge_params = rust_backend_lib.set_rand_edge_params_ext
+    set_rand_edge_params.argtypes = [C.c_void_p, C.c_float, C.c_float]
+    all_funcs.update({"set_rand_edge_params": set_rand_edge_params})
