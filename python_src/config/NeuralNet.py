1236110705f311c79d910209bb88c9af987a678f
Cy63rPh4n70m <103024336+Cy63rPh4n70m@users.noreply.github.com>
2025-11-27 01:15:00 +0800

    Read hyperparameters from JSON file
    JSON files are used to pass the hyperparameters of
    neural nets for convenience.

--- python_src/config/NeuralNet.py
+++ python_src/config/NeuralNet.py
@@ -1,3 +1,20 @@
+import json
+
 class NeuralNet:
     def __init__(self, hyper_param_json_path: str):
-        pass
+        with open(hyper_param_json_path, "r") as f:
+            hyper_params = json.load(f)
+
+        self.in_dim: int = hyper_params["in_dim"]
+        self.out_dim: int = hyper_params["out_dim"]
+        self.n_input_neurons: int = hyper_params["n_input_neurons"]
+        self.n_output_neurons: int = hyper_params["n_output_neurons"]
+        self.max_edges: int = hyper_params["max_edges"]
+        self.max_depth: int = hyper_params["max_depth"]
+
+        self.add_neuron_rate: float = hyper_params["add_neuron_rate"]
+        self.remove_neuron_rate: float = hyper_params["remove_neuron_rate"]
+        self.add_io_edge_rate: float = hyper_params["add_io_edge_rate"]
+        self.join_neuron_rate: float = hyper_params["join_neuron_rate"]
+        self.remove_edge_rate: float = hyper_params["remove_edge_rate"]
+        self.edge_param_thresh: float = hyper_params["edge_param_thresh"]
