3dca74093ae8133f5f29d370f959b3ac93c4eb3f
Cy63rPh4n70m <103024336+Cy63rPh4n70m@users.noreply.github.com>
2025-11-26 11:29:15 +0800

    Add ctypes function for spawning threads

---
+++ python_src/config/ctypes_funcs/spawn_threads.py
@@ -0,0 +1,13 @@
+import ctypes as C
+from ctypes import CDLL
+
+def get_spawn_threads_func(rust_backend_lib: CDLL, all_funcs: dict[str, C._NamedFuncPointer]):
+    """
+    Function to spawn threads for parallel propagation in neural net via Breadth
+    First Search.
+    """
+
+    # Args: nn_vp, num_threads, lr, return_grad
+    spawn_threads = rust_backend_lib.spawn_threads_ext
+    spawn_threads.argtypes = [C.c_void_p, C.c_uint64, C.c_float, C.c_bool]
+    all_funcs.update({"spawn_threads": spawn_threads})
