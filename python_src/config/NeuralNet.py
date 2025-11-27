ec68343513b26c9011b7f0e0a5d7edbdf6712785
Cy63rPh4n70m <103024336+Cy63rPh4n70m@users.noreply.github.com>
2025-11-27 22:16:21 +0800

    Obtain the output array of the forward method

--- python_src/config/NeuralNet.py
+++ python_src/config/NeuralNet.py
@@ -70,4 +70,8 @@ class NeuralNet:
         self.rust_backend_funcs["prop_forward"](self.nn_vp, True)
         self.rust_backend_funcs["set_input_vec"](self.nn_vp, array, self.in_dim)
         self.rust_backend_funcs["init_output_vecs"](self.nn_vp, self.out_dim)
-        self.rust_backend_funcs["propagate"](self.nn_vp)
+        output_array_ptr = self.rust_backend_funcs["propagate"](self.nn_vp)
+        output_array = np.ctypeslib.as_array(output_array_ptr, (self.out_dim,))
+        self.rust_backend_funcs["free_vec"](output_array_ptr, self.out_dim)
+
+        return output_array
