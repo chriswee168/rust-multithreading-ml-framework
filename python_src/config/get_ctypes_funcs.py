79b5545a200cebc7f3bdf83618199da8862402bf
Cy63rPh4n70m <103024336+Cy63rPh4n70m@users.noreply.github.com>
2025-11-29 19:29:37 +0800

    Create ctypes function for displaying parameters

--- python_src/config/get_ctypes_funcs.py
+++ python_src/config/get_ctypes_funcs.py
@@ -21,5 +21,6 @@ def get_ctypes_funcs(lib_path: str) -> dict:
     remove_neuron.get_remove_neuron_func(rust_backend_lib, all_funcs)
     set_edge_params.get_set_edge_params_func(rust_backend_lib, all_funcs)
     spawn_threads.get_spawn_threads_func(rust_backend_lib, all_funcs)
+    display_params.get_display_params_func(rust_backend_lib, all_funcs)
 
     return all_funcs
