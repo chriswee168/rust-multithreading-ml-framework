6071ec6f9269a557ad690dd0458a5c8f6d8e97be
Cy63rPh4n70m <103024336+Cy63rPh4n70m@users.noreply.github.com>
2025-11-30 10:22:43 +0800

    Create method to obtain a valid neuron
    This method is required when adding new hidden neurons to
    to ensure that they have one backward and forward
    edge to contribute to the neural network's output.

--- rust_src/src/extern_funcs_src/add_neurons.rs
+++ rust_src/src/extern_funcs_src/add_neurons.rs
@@ -73,3 +73,82 @@ pub extern "C" fn add_output_neuron_ext(
         (*nn_ptr).add_output_neuron(id_str, neuron);
     }
 }
+
+/// Conditionally select existing neuron from the neural network
+/// to connect with a newly added one.
+fn obtain_valid_neuron(
+    target_neuron_id: &String,
+    target_neuron_level: u32,
+    neuron_group1: &HashMap<String, ArcNeuronTrait>,
+    neuron_group2: &HashMap<String, ArcNeuronTrait>,
+    connect_forward: bool,
+    mut rand_gen: &mut rand::prelude::ThreadRng,
+) -> String
+{
+    let selected_neuron_id: String;
+
+    let (mut neuron_id_op, mut neuron_op);
+    let mut neuron_id: String;
+
+    loop
+    {
+        if rand_gen.gen_bool(0.5)
+        {
+            (neuron_id_op, neuron_op) = get_rand_neuron(
+                neuron_group1, &mut rand_gen
+            )
+        }
+        else
+        {
+            (neuron_id_op, neuron_op) = get_rand_neuron(
+                neuron_group2, &mut rand_gen
+            )
+        }
+
+        if neuron_op.is_some()
+        {
+            neuron_id = neuron_id_op.unwrap();
+            
+            if neuron_id != *target_neuron_id
+            {
+                let neuron: ArcNeuronTrait = neuron_op.unwrap();
+                let neuron_guard: MutexGuard<'_, Box<dyn NeuronTrait>> = neuron.lock().unwrap();
+                let neuron_level: Option<u32> = neuron_guard.get_neuron_level();
+                
+                if neuron_level.is_some()
+                {
+                    let neuron_is_valid: bool;
+                    let under_max_edges: bool;
+                    
+                    // Either checking to connect a forward or backward edge.
+                    if connect_forward
+                    {
+                        // Next neuron must have higher level.
+                        neuron_is_valid = target_neuron_level < neuron_guard.get_neuron_level().unwrap();
+                        under_max_edges = neuron_guard.get_forward_edges().len() < neuron_guard.get_forward_edge_max();
+                    }
+                    else
+                    {
+                        // Previous neuron must have lower level.
+                        neuron_is_valid = target_neuron_level > neuron_guard.get_neuron_level().unwrap();
+                        under_max_edges = neuron_guard.get_backward_edges().len() < neuron_guard.get_backward_edge_max();
+                    }
+
+                    if neuron_is_valid && under_max_edges
+                    {
+                        selected_neuron_id = neuron_id;
+                        break;
+                    }
+                }
+                else
+                {
+                    // Neuron has no level, indicating its an input/output neuron.
+                    selected_neuron_id = neuron_id;
+                    break;
+                }
+            }
+        }
+    }
+
+    return selected_neuron_id;
+}
