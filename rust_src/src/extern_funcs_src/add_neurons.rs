57a850a0b32f83081e328686d2299fde4f34774f
Cy63rPh4n70m <103024336+Cy63rPh4n70m@users.noreply.github.com>
2025-12-01 09:33:16 +0800

    WIP: Remove looping
    Prevent the function from looping endlessly when
    no valid neuron is selectable.

--- rust_src/src/extern_funcs_src/add_neurons.rs
+++ rust_src/src/extern_funcs_src/add_neurons.rs
@@ -125,59 +125,55 @@ fn obtain_valid_neuron(
     let (mut neuron_id_op, mut neuron_op);
     let mut neuron_id: String;
 
-    loop
+    if rand_gen.gen_bool(0.5)
     {
-        if rand_gen.gen_bool(0.5)
-        {
-            (neuron_id_op, neuron_op) = get_rand_neuron(
-                neuron_group1, &mut rand_gen
-            )
-        }
-        else
-        {
-            (neuron_id_op, neuron_op) = get_rand_neuron(
-                neuron_group2, &mut rand_gen
-            )
-        }
+        (neuron_id_op, neuron_op) = get_rand_neuron(
+            neuron_group1, &mut rand_gen
+        )
+    }
+    else
+    {
+        (neuron_id_op, neuron_op) = get_rand_neuron(
+            neuron_group2, &mut rand_gen
+        )
+    }
 
-        if neuron_op.is_some()
-        {
-            neuron_id = neuron_id_op.unwrap();
+    if neuron_op.is_some()
+    {
+        neuron_id = neuron_id_op.unwrap();
             
-            if neuron_id != *target_neuron_id
-            {
-                let neuron: ArcNeuronTrait = neuron_op.unwrap();
-                let neuron_guard: MutexGuard<'_, Box<dyn NeuronTrait>> = neuron.lock().unwrap();
-                let neuron_level: Option<u32> = neuron_guard.get_neuron_level();
+        if neuron_id != *target_neuron_id
+        {
+            let neuron: ArcNeuronTrait = neuron_op.unwrap();
+            let neuron_guard: MutexGuard<'_, Box<dyn NeuronTrait>> = neuron.lock().unwrap();
+            let neuron_level: Option<u32> = neuron_guard.get_neuron_level();
                 
-                let mut neuron_is_valid: bool = true;
-                let under_max_edges: bool;
+            let mut neuron_is_valid: bool = true;
+            let under_max_edges: bool;
                     
-                // Either checking to connect a forward or backward edge.
-                if connect_forward
+            // Either checking to connect a forward or backward edge.
+            if connect_forward
+            {
+                if neuron_level.is_some()
                 {
-                    if neuron_level.is_some()
-                    {
-                        // Next neuron must have higher level.
-                        neuron_is_valid = target_neuron_level < neuron_guard.get_neuron_level().unwrap();
-                    }
-                    under_max_edges = neuron_guard.get_forward_edges().len() < neuron_guard.get_forward_edge_max();
+                    // Next neuron must have higher level.
+                    neuron_is_valid = target_neuron_level < neuron_guard.get_neuron_level().unwrap();
                 }
-                else
+                under_max_edges = neuron_guard.get_forward_edges().len() < neuron_guard.get_forward_edge_max();
+            }
+            else
+            {
+                if neuron_level.is_some()
                 {
-                    if neuron_level.is_some()
-                    {
-                        // Previous neuron must have lower level.
-                        neuron_is_valid = target_neuron_level > neuron_guard.get_neuron_level().unwrap();
-                    }
-                    under_max_edges = neuron_guard.get_backward_edges().len() < neuron_guard.get_backward_edge_max();
+                    // Previous neuron must have lower level.
+                    neuron_is_valid = target_neuron_level > neuron_guard.get_neuron_level().unwrap();
                 }
+                under_max_edges = neuron_guard.get_backward_edges().len() < neuron_guard.get_backward_edge_max();
+            }
 
-                if neuron_is_valid && under_max_edges
-                {
-                    selected_neuron_id = neuron_id;
-                    break;
-                }
+            if neuron_is_valid && under_max_edges
+            {
+                selected_neuron_id = neuron_id;
             }
         }
     }
