ea09d3f5e9a71c6db35eeb76ad50c5fd93d365d6
Cy63rPh4n70m <103024336+Cy63rPh4n70m@users.noreply.github.com>
2025-12-05 22:10:49 +0800

    Use ceil instead of adding 1
    The neurons_per_buffer variable is only rounded
    up if its not a whole number to prevent some threads
    from not being given any neurons to work on.

--- rust_src/src/neural_net_wrapper_src/neural_net_wrapper.rs
+++ rust_src/src/neural_net_wrapper_src/neural_net_wrapper.rs
@@ -130,7 +130,7 @@ impl NeuralNetWrapper
         num_io_neurons = io_neurons.len();
 
         let num_threads: usize = self.thread_handles.len();
-        let neurons_per_buffer: usize = (num_io_neurons / num_threads) + 1;
+        let neurons_per_buffer: usize = (num_io_neurons as f32 / num_threads as f32).ceil() as usize;
 
         let mut increment: usize = 0;
         let mut buffer_guard_idx: usize = 0;
@@ -155,7 +155,10 @@ impl NeuralNetWrapper
             {
                 // Obtain the next buffer.
                 buffer_guard_idx += 1;
-                buffer = &mut buffers[buffer_guard_idx];
+                if buffer_guard_idx < self.thread_handles.len()
+                {
+                    buffer = &mut buffers[buffer_guard_idx];
+                }
                 increment = 0;
             }
         }
