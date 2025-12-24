564e6cfb8228c00ec3393152a69d3ff6f510c748
Cy63rPh4n70m <103024336+Cy63rPh4n70m@users.noreply.github.com>
2025-12-24 19:32:13 +0800

    Call save_edges in save_model function

--- rust_src/src/extern_funcs_src/save_load_model.rs
+++ rust_src/src/extern_funcs_src/save_load_model.rs
@@ -1,6 +1,6 @@
 use std::ffi::{c_char, c_void, CStr};
 
-use crate::{load_save_src::save::save_neurons, neural_net_wrapper_src::neural_net_wrapper::NeuralNetWrapper};
+use crate::{load_save_src::save::{save_edges, save_neurons}, neural_net_wrapper_src::neural_net_wrapper::NeuralNetWrapper};
 
 /// Save the parameters of neural network.
 #[unsafe(no_mangle)]
@@ -14,5 +14,10 @@ pub extern "C" fn save_model_ext(nn_vp: *mut c_void, model_dir_str: *mut c_char)
         save_neurons(&(*neural_net).neural_net.input_neurons, model_dir.clone() + "/input_neurons.json");
         save_neurons(&(*neural_net).neural_net.hidden_neurons, model_dir.clone() + "/hidden_neurons.json");
         save_neurons(&(*neural_net).neural_net.output_neurons, model_dir.clone() + "/output_neurons.json");
+
+        // Save input, hidden and output edges.
+        save_edges(&(*neural_net).neural_net.input_edges, model_dir.clone() + "/input_edges.json", "input");
+        save_edges(&(*neural_net).neural_net.hidden_edges, model_dir.clone() + "/hidden_edges.json", "hidden");
+        save_edges(&(*neural_net).neural_net.output_edges, model_dir.clone() + "/output_edges.json", "output");
     }
 }
