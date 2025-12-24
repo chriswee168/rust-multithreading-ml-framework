7629d15c1d38787d91734133ab9c1cb7c04680f2
Cy63rPh4n70m <103024336+Cy63rPh4n70m@users.noreply.github.com>
2025-12-24 12:19:42 +0800

    Move save_model to extern_funcs_src

---
+++ rust_src/src/extern_funcs_src/save_load_model.rs
@@ -0,0 +1,18 @@
+use std::ffi::{c_char, c_void, CStr};
+
+use crate::{load_save_src::save::save_neurons, neural_net_wrapper_src::neural_net_wrapper::NeuralNetWrapper};
+
+/// Save the parameters of neural network.
+#[unsafe(no_mangle)]
+pub extern "C" fn save_model_ext(nn_vp: *mut c_void, model_dir_str: *mut c_char)
+{
+    unsafe
+    {
+        let neural_net: *mut NeuralNetWrapper = nn_vp as *mut NeuralNetWrapper;
+        let model_dir: String = CStr::from_ptr(model_dir_str).to_str().unwrap().to_string();
+        // Save input, hidden and output neurons.
+        save_neurons(&(*neural_net).neural_net.input_neurons, model_dir.clone() + "/input_neurons.json");
+        save_neurons(&(*neural_net).neural_net.hidden_neurons, model_dir.clone() + "/hidden_neurons.json");
+        save_neurons(&(*neural_net).neural_net.output_neurons, model_dir.clone() + "/output_neurons.json");
+    }
+}
