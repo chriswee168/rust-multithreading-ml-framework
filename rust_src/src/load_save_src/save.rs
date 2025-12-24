0e28e81421479565da2dc354350f4a02f76b06c1
Cy63rPh4n70m <103024336+Cy63rPh4n70m@users.noreply.github.com>
2025-12-24 11:07:45 +0800

    Create save_neurons function
    Iteratively save the details of each neuron to target JSON file.

---
+++ rust_src/src/load_save_src/save.rs
@@ -0,0 +1,35 @@
+/// Obtain each neuron and write data to json file.
+fn save_neurons(neuron_hashmap: &HashMap<String, ArcNeuronTrait>, json_path: String)
+{
+    let json_file: Result<File, std::io::Error> = 
+        OpenOptions::new().create(true).append(true).open(json_path);
+    
+    if json_file.is_err()
+    {
+        println!("{}", json_file.err().unwrap())
+    }
+
+    else
+    {
+        let mut json_file: File = json_file.unwrap();
+        for (_, neuron) in neuron_hashmap
+        {
+            let guard: MutexGuard<'_, Box<dyn NeuronTrait>> = neuron.lock().unwrap();
+            // Obtain all necessary neuron info.
+            let json_entry = json!({
+                "backward_edge_max": guard.get_backward_edge_max(),
+                "forward_edge_max": guard.get_forward_edge_max(),
+                "neuron_id": guard.get_neuron_id(),
+                "neuron_level": guard.get_neuron_level()
+            });
+            
+            // Write to JSON file.
+            let json_str: String = serde_json::to_string(&json_entry).unwrap();
+            let result: Result<(), std::io::Error> = json_file.write_all(json_str.as_bytes());
+            if result.is_err()
+            {
+                println!("{}", result.err().unwrap())
+            }
+        }
+    }
+}
