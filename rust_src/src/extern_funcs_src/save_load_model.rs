7c41a66381d9a4f5c3f542c2c3e6e301ab81036a
Cy63rPh4n70m <103024336+Cy63rPh4n70m@users.noreply.github.com>
2025-12-25 22:46:40 +0800

    Create load_model_ext function

--- rust_src/src/extern_funcs_src/save_load_model.rs
+++ rust_src/src/extern_funcs_src/save_load_model.rs
@@ -1,6 +1,6 @@
 use std::ffi::{c_char, c_void, CStr};
 
-use crate::{load_save_src::save::{save_edges, save_neurons}, neural_net_wrapper_src::neural_net_wrapper::NeuralNetWrapper};
+use crate::{load_save_src::{load::{load_edges, load_neurons}, save::{save_edges, save_neurons}}, neural_net_wrapper_src::neural_net_wrapper::NeuralNetWrapper};
 
 /// Save the parameters of neural network.
 #[unsafe(no_mangle)]
@@ -19,5 +19,24 @@ pub extern "C" fn save_model_ext(nn_vp: *mut c_void, model_dir_str: *mut c_char)
         save_edges(&(*neural_net).neural_net.input_edges, model_dir.clone() + "/input_edges.json", "input");
         save_edges(&(*neural_net).neural_net.hidden_edges, model_dir.clone() + "/hidden_edges.json", "hidden");
         save_edges(&(*neural_net).neural_net.output_edges, model_dir.clone() + "/output_edges.json", "output");
+    }
+}
+
+/// Load parameters into an empty neural network.
+#[unsafe(no_mangle)]
+pub extern "C" fn load_model_ext(nn_vp: *mut c_void, model_dir_str: *mut c_char)
+{
+    unsafe
+    {
+        let model_dir: String = CStr::from_ptr(model_dir_str).to_str().unwrap().to_string();
+        // Load input, hidden and output neurons.
+        load_neurons(nn_vp, model_dir.clone() + "/input_neurons.json", "input");
+        load_neurons(nn_vp, model_dir.clone() + "/hidden_neurons.json", "output");
+        load_neurons(nn_vp, model_dir.clone() + "/output_neurons.json", "hidden");
+
+        // Load input, hidden and output edges and connect the neurons.
+        load_edges(nn_vp, model_dir.clone() + "/input_edges.json", "input");
+        load_edges(nn_vp, model_dir.clone() + "/hidden_edges.json", "hidden");
+        load_edges(nn_vp, model_dir.clone() + "/output_edges.json", "output");
     }
 }
